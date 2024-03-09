024fdd38f7f7b87af728395e2874d970b923f858
Twisterday <garayda.rostik@gmail.com>
2024-03-08 19:28:56 -0600

    Add logo to dashboard

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -1,14 +1,31 @@
+import { useTheme } from "@mui/material/styles";
+import { Box } from "@mui/material"
 import React from "react";
 import { Outlet } from "react-router-dom";
 
+import Logo from "../../assets/Images/logo.ico";
+
 const DashboardLayout = () => {
 
-  return (
-    <>
-      Dashboard Layout
-      <Outlet />
-    </>
-  );
+	const theme = useTheme();
+
+	console.log(theme);
+
+	return (
+		<>
+			<Box sx={{ backgroundColor: theme.palette.background.paper, boxShadow: "0px 0px 2px rgba(0, 0, 0, 0.25)", height: "100vh", width: "100px" }}>
+				<Box sx={{
+					backgroundColor: theme.palette.primary.main,
+					height: 64,
+					width: 64,
+					borderRadius: 1.5
+				}}>
+					<img src={Logo} alt={"Yet Logo"} />
+				</Box>
+			</Box >
+			<Outlet />
+		</>
+	);
 };
 
 export default DashboardLayout;
